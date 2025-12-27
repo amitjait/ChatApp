9098b5bb9ec86232813078a6d9757935ccc603e2
Amit Kumar <amitkumar@Amits-MacBook-Air-2.local>
2025-12-27 22:36:45 +0530

    Fixed db.json issue in db.js

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,7 +1,12 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
-const DB_PATH = path.resolve("src/database/db.json");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// db.json is in the SAME folder as this file
+const DB_PATH = path.join(__dirname, "db.json");
 
 export const readDB = () => {
   const data = fs.readFileSync(DB_PATH, "utf-8");
